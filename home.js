896937f337e31f14e4306fe1e2c2381471377030
dlghwjd-tw <83802103+dlghwjd-tw@users.noreply.github.com>
2023-04-11 22:39:38 +0800

    Add files via upload

--- home.js
+++ home.js
@@ -19,17 +19,22 @@ window.addEventListener('DOMContentLoaded', function() {
     })
 
     function countdown() {
-        var date = Math.floor(Date.now() / 1000)
-        var sec = date % 60
-        var min = (date - sec) / 60
-        var hrs = (min - min % 60) / 60
-        var day = (hrs - hrs % 24) / 24
-        var startDate = new Date(2018, 4, 12) / 1000
-        var secForStartDate = startDate % 60
-        var minForStartDate = (startDate - secForStartDate) / 60
-        var hrsForStartDate = (minForStartDate - minForStartDate % 60) / 60
-        var dayForStartDate = (hrsForStartDate - hrsForStartDate % 24) / 24
-        countdownEle.innerHTML = `與BB豬已一起渡過${day - dayForStartDate}日`
+        var currentDate = Math.floor(Date.now());
+        var startDate = new Date(2018, 4, 12);
+        var diff = new Date(currentDate - startDate); 
+        var daysPassed = diff.getDate() - 1; 
+        var monthsPassed = diff.getMonth();
+        var yearsPassed = diff.getFullYear() - 1970; 
+        var totalDays = Math.ceil(diff / (1000 * 60 * 60 * 24)); 
+        if (daysPassed == 0 && monthsPassed == 0) {
+            countdownEle.innerHTML = `與BB豬已一起渡過第${yearsPassed}年紀念日（第${totalDays}日）`;
+        } else if (monthsPassed == 0) {
+            countdownEle.innerHTML = `與BB豬已一起渡過${yearsPassed}年零${daysPassed}日（第${totalDays}日）`;
+        } else if (daysPassed == 0) {
+            countdownEle.innerHTML = `與BB豬已一起渡過${yearsPassed}年零${monthsPassed}個月（第${totalDays}日）`;
+        } else {
+            countdownEle.innerHTML = `與BB豬已一起渡過${yearsPassed}年${monthsPassed}個月零${daysPassed}日（第${totalDays}日）`;
+        }
     }
     countdown()
     setInterval(() => {
